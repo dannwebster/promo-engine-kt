8462c3052edcdfabf8a1b87e6075bf90e279ab05
Dann Webster <dannwebster@gmail.com>
2016-11-06 10:38:54 -0800

    added promo dao

--- src/main/kotlin/com/fandango/data/PromoDao.kt
+++ src/main/kotlin/com/fandango/data/PromoDao.kt
@@ -4,13 +4,14 @@ import org.springframework.jdbc.core.JdbcTemplate
 import java.sql.ResultSet
 
 class PromoDao(val jdbcTemplate: JdbcTemplate) {
-    val QUERY_JOIN = """
+    val QUERY_JOIN =
+            """
             SELECT p.promo_guid, p.promo_id, c.campaign_id, c.name
             FROM promos AS p
             INNER JOIN campaigns AS c
             ON c.campaign_id = p.campaign_id
             WHERE p.promo_guid = ?
-"""
+            """
 
     fun getPromoById(promoGuid: String) = jdbcTemplate.query(QUERY_JOIN, arrayOf(promoGuid))
         { rs, rownum -> mapPromo(rs) }
